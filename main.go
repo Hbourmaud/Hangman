d2d5d0dbdc2f0be675bf308d3b9334cd15e0bb49
HBOURMAUD <hugo.bourmaud@ynov.com>
2021-10-06 11:49:26 +0200

    WordChoose

--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	fmt.Println(WordChoose(1))
+}
+
+func WordChoose(n int) []byte {
+	cpt := 0
+	cptmot := 0
+	index := 0
+	args := os.Args[1]
+	var word []byte
+	listword, _ := os.Open(args)
+	info, _ := os.Stat(args)
+	size := info.Size()
+	arr := make([]byte, size)
+	listword.Read(arr)
+	listword.Close()
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 13 {
+			cptmot++
+		}
+	}
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 13 {
+			cpt++
+			if n == 1 {
+				word = arr[0:i]
+				return word
+			}
+			if cpt == n {
+				word = arr[index+2 : i]
+				return word
+			}
+			if cpt == n-1 && cptmot+1 == n {
+				var temp []byte
+				word = arr[index+2:]
+				for k := 0; k < len(word); k++ {
+					if word[k] == 10 {
+						temp = word[k+1:]
+					}
+				}
+				return temp
+			}
+			index = i
+		}
+	}
+	return word
 }
